07726729c1712af39a942f0e94b46f0cb8a2950f
Júlia D'Almeida Gaboardi <121109163+juliadga@users.noreply.github.com>
2026-04-13 17:09:18 -0300

    registrarEscolha in index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,3 +8,11 @@ export function setRoutes(app) {
     app.use('/', router);
     router.get('/', indexController.getIndex.bind(indexController));
 }
+
+const express = require('express');
+const router = express.Router();
+const escolhaController = require('../controllers/escolhaController');
+
+router.post('/', escolhaController.registrarEscolha);
+
+module.exports = router;
